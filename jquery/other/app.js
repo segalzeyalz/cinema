c393075b30e729461ae43006c458b1243e9e39c8
EYAL Segal <segaleseo@gmail.com>
2017-12-05 08:13:20 +0200

    adding

---
+++ jquery/other/app.js
@@ -0,0 +1,4 @@
+$('div').on('click', function(){
+  $('#one').empty();
+  $('#two').remove();  
+})
--- jquery/pet/app.js
+++ jquery/pet/app.js
@@ -1,26 +1,42 @@
 $('.loc').hover(
   function(){
-    $(this).html("<strong>Location:</strong> Your house?")
+    $(this).html("<strong>Location:</strong> Your house?!");
   },
-  function(){
-    $(this).html("<strong>Location:</strong> Treehouse Adoption Center?")
-  })
-$('#add-pet').on('click', function(){
-  // Grab info from the form
-  let $name =  $('#pet-name');
-  let $species =  $('#pet-species');
-  let $notes =  $('#pet-notes');
+  function() {
+    $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
+});
 
-  let $newPet = $(
-    '<section class="six columns"><div class="card><p><strong>Name:</strong>' + $name.val() +
-    '</p><p><strong>Species:</strong>' + $species.val() +
-    '</p><p><strong>Notes:</strong>' + $notes.val() +  
-    '</p><span class="close">&times;</span></div></section>')
-    $('#posted-pets').append($newPet);
-  })
+// Adds a pet to the page with user input on button click
+$('#add-pet').on('click', function() {
+  // Grab info from the form
+  var $name = $('#pet-name');
+  var $species = $('#pet-species');
+  var $notes = $('#pet-notes');
   
-  $('img').css('display', 'none').fadeIn(500);
-  $('.card').on('click', function(){
-    $(this).toggleClass('selected');
-  })
+  //Assemble the HTML of our new element with the above variables
+  var $newPet = $(
+      '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
+      '</p><p><strong>Species:</strong> ' + $species.val() +
+      '</p><p><strong>Notes:</strong> ' + $notes.val() +
+      '</p><span class="close">&times;</span></div></section>'
+    );
   
+  // Attach the new element to the page
+  $('#posted-pets').append($newPet);
+  
+  // Reset form fields
+  $name.val("");
+  $species.val("Dog");
+  $notes.val("");
+  $('.close').on('click', function(){
+    $(this).parent().remove();
+  })
+});
+
+// Puppy images fade in
+$('img').css('display', 'none').fadeIn(1600);
+
+
+$('.card').on('click', function() {
+  $(this).toggleClass('selected');
+});
